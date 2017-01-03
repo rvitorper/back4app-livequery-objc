b36a9b1b8d24b19b120792ee79f2b736e9247063
Ayrton Alves <ayr.ton10@hotmail.com>
2017-01-03 17:38:48 -0200

    Initial Commit

---
+++ LiveQueryExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveQueryExample
+//
+//  Created by Ayrton Alves on 03/01/17.
+//  Copyright © 2017 back4app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveQueryExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveQueryExample
+//
+//  Created by Ayrton Alves on 03/01/17.
+//  Copyright © 2017 back4app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
